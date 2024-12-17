c567ed611b665d2c4a9e8f7c84848df39020d214
Harshada Mane <harshada@gmaul.com>
2024-12-17 16:34:47 +0530

    added empoyee . jav ain main

---
+++ main_test/employee.java
@@ -0,0 +1,20 @@
+package sample;
+//made this chnage from main brach
+public class employee {
+
+	int empsalary;
+	String empname;
+	
+	public employee(int empsalary, String empname)
+	{
+		this.empname = empname;
+		this.empsalary=empsalary;
+		
+	}
+	
+	public String toString()
+	{
+		return "["+this.empname+","+this.empsalary+"]";
+	
+	}
+}
